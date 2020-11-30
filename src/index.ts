e7ed308602e759cdc470c21404ec4bcb6fd89fd7
Reto Lamprecht <reto.lamprecht@gmx.ch>
2020-11-30 22:35:18 +0100

    update tsconfig.json and package.json

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+export * from "./geometry-2d/Circle";
+export * from "./geometry-2d/Dimensions";
+export * from "./geometry-2d/Line";
+export * from "./geometry-2d/Margin";
+export * from "./geometry-2d/Offset";
+export * from "./geometry-2d/Point";
+export * from "./geometry-2d/Rectangle";
+export * from "./geometry-2d/Square";
+export * from "./geometry-2d/Transform";
+
+
 import { runDemo } from './demos/demo';
 
 window.onload = () => {
